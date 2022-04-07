9aa9d9a37fb4a05e5094d6b3a64d6ae4465abfbd
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-04-06 22:36:32 -0300

    buton back func

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="md: flex flex-col content-center text-white">
       <div class="md: flex flex-row justify-between">
-        <button class="w-20 border-2 border-white rounded-md hover:bg-white hover:text-green-800 text-xl font-bold">Back</button>
+        <button  class="w-20 border-2 border-white rounded-md hover:bg-white hover:text-green-800 text-xl font-bold"
+        v-on:click="goback">
+          Back 
+        </button>
         <h1 class="font-bold">Vue MusicApp</h1>
       </div>
       <div class="md:flex flex-col content-center justify-center mt-4 pr-2">
@@ -12,7 +15,7 @@
       </div>
 
       <div class="md:flex flex-row content-center justify-center space-x-12 pt-8 mr-8">
-        <button class="border-2 border-white rounded-full h-14 w-14 bg-white pt-4"><img src="" class="" alt=""></button>
+        <button class="border-2 border-white rounded-full h-14 w-14 bg-white pt-4"></button>
         <button class="border-2 border-white rounded-full h-14 w-14 pl-4 hover:bg-green-800 hover:border-green-800 bg-white"><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-pause-512.png" class="h-6 w-6" alt=""></button>
         <button class="border-2 border-white rounded-full h-14 w-14 pl-4 bg-white hover:bg-black"><img src="https://findicons.com/files/icons/770/token_dark/256/media_player.png" class="h-6 w-6" alt=""></button>
       </div>
@@ -30,11 +33,15 @@ export default {
       albumName: String,
       year: Number,
       src: String,
-      songSrc: String
+      songSrc: String,
     },
-    methods: {
-      
-    }
+
+  },
+  emits: ['goback'],
+  methods: {
+      goback(){
+        this.$emit('goback');
+      }
   }
 }
 </script>
